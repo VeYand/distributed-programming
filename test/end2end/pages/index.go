a98db395d083659d9ebaf583067373fbc1ed3872
VeYand <ValerikLerik17@gmail.com>
2025-05-15 10:44:18 +0300

    Выпилил неиспользуемый код

--- test/end2end/pages/index.go
+++ test/end2end/pages/index.go
@@ -38,25 +38,6 @@ func (c *Page) WaitForElement(by, value string, timeout time.Duration) (selenium
 	return nil, fmt.Errorf("element not found after %v: %v", timeout, err)
 }
 
-func (c *Page) WaitForElements(by, value string, timeout time.Duration) ([]selenium.WebElement, error) {
-	var elems []selenium.WebElement
-	var err error
-
-	startTime := time.Now()
-	for time.Since(startTime) < timeout {
-		elems, err = c.Driver.FindElements(by, value)
-		if err == nil && len(elems) > 0 {
-			return elems, nil
-		}
-		time.Sleep(500 * time.Millisecond)
-	}
-	return nil, fmt.Errorf("elements not found after %v: %v", timeout, err)
-}
-
 func (c *Page) FindElement(by, value string) (selenium.WebElement, error) {
 	return c.WaitForElement(by, value, 10*time.Second)
-}
-
-func (c *Page) FindElements(by, value string) ([]selenium.WebElement, error) {
-	return c.WaitForElements(by, value, 10*time.Second)
 }
